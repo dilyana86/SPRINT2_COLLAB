4be4dadb72766793bf3853220192a5a95e5b6531
dilyana86 <dilyana86stoyanova@gmail.com>
2023-10-28 21:36:20 -0400

    Day1 set up with team

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,53 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = {
+                    "html:target/cucumber-reports.html",
+                    "rerun:target/rerun.txt",
+                    "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+            },
+            features = "src/test/resources/features",
+            glue = "com/cydeo/step_definitions",
+            dryRun = false,                   //by default is false, true is only for snippets
+            tags = "",
+            publish = true
+
+    )
+
+public class CukesRunner {
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
